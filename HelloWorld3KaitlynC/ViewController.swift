9a7b2288dec3c5cccd8075d52e4be0ae3c59af61
KaitlynClay <122916928+KaitlynClay@users.noreply.github.com>
2024-01-24 23:07:07 -0600

    Initial Commit

---
+++ HelloWorld3KaitlynC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld3KaitlynC
+//
+//  Created by student on 1/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
