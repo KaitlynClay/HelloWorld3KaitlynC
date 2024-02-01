b03ffc0251929477d7b350a1332074502dedb309
KaitlynClay <122916928+KaitlynClay@users.noreply.github.com>
2024-02-01 10:40:29 -0600

    almost working..

--- HelloWorld3KaitlynC/ViewController.swift
+++ HelloWorld3KaitlynC/ViewController.swift
@@ -8,19 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var labelResult: UILabel!
 
+    @IBOutlet var textMessage: UITextField!
+    
+    
+    @IBAction func changeButton(_ sender: UIButton) {
+        labelResult.text = textMessage.text?.uppercased()
+    }
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        let label = UILabel()
-        label.frame = CGRect(x:125, y:125, width: 200, height: 80)
-        label.text = "This is a label"
-        view.addSubview(label)
-        
-        let button = UIButton()
-        button.frame = CGRect(x: 125, y: 300, width: 80, height: 80)
-        button.setTitle("Button", for: .normal)
-        button.setTitleColor(UIColor.blue, for: .normal)
-        view.addSubview(button)
+//        super.viewDidLoad()
+//        let label = UILabel()
+//        label.frame = CGRect(x:125, y:125, width: 200, height: 80)
+//        label.text = "This is a label"
+//        view.addSubview(label)
+//
+//        let button = UIButton()
+//        button.frame = CGRect(x: 125, y: 300, width: 80, height: 80)
+//        button.setTitle("Button", for: .normal)
+//        button.setTitleColor(UIColor.blue, for: .normal)
+//        view.addSubview(button)
+//
+//
+//
     }
 
 
