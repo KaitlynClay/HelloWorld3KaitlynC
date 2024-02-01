1aa4ed10430f20d411b3d9e28e1f957fe9f09f23
KaitlynClay <122916928+KaitlynClay@users.noreply.github.com>
2024-02-01 17:02:30 -0600

    working!

--- HelloWorld3KaitlynC/ViewController.swift
+++ HelloWorld3KaitlynC/ViewController.swift
@@ -10,32 +10,15 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var labelResult: UILabel!
-
-    @IBOutlet weak var textMessage: UITextField!
     
+    @IBOutlet weak var txtmessage: UITextField!
     
     @IBAction func changeButton(_ sender: UIButton) {
-        labelResult.text = textMessage.text?.uppercased()
+        labelResult.text = txtmessage.text?.uppercased()
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let label = UILabel()
-//        label.frame = CGRect(x:125, y:125, width: 200, height: 80)
-//        label.text = "This is a label"
-//        view.addSubview(label)
-//
-//        let button = UIButton()
-//        button.frame = CGRect(x: 125, y: 300, width: 80, height: 80)
-//        button.setTitle("Button", for: .normal)
-//        button.setTitleColor(UIColor.blue, for: .normal)
-//        view.addSubview(button)
-//
-//
-//
     }
-
-
 }
 
